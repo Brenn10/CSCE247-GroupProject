99b386ccb6f30f243f9809fec3f2ce480371d9b6
Brennan Cain <brenn.cain@gmail.com>
2021-10-19 08:56:02 -0400

    Fix JSON UUID

--- src/utilities/UUIDGen.java
+++ src/utilities/UUIDGen.java
@@ -3,13 +3,10 @@ package utilities;
 import java.util.UUID;
 
 public class UUIDGen {
-    public static int getIntUUID() {
-        return Math.abs((int)UUID.randomUUID().getLeastSignificantBits());
-    }
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println(getIntUUID());
+            System.out.println(UUID.randomUUID());
         }
     }
 }
