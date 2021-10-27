c524a148c060c4009960136363c2d0cafb832a89
Robert Sturman <tssturman@gmail.com>
2021-10-27 11:21:48 -0400

    Added TODO signup, removed Admin method disguise

--- src/AdminUI.java
+++ src/AdminUI.java
@@ -20,16 +20,13 @@ public class AdminUI {
         System.out.println("Welcome " + admin.getFirstName() + " " + admin.getLastName());
         boolean doMenu = true;
         while (doMenu) {
-            System.out.println("(1) Access as another user"); // how would this be stated? go to user X and edit as if they're that person
-            System.out.println("(2) Enter User Approval Mode");
+            //System.out.println("(1) Access as another user"); // how would this be stated? go to user X and edit as if they're that person
+            System.out.println("(1) Enter User Approval Mode");
             System.out.println("(0) Exit Neurotic Job Search");
             System.out.print("What would you like to do: ");
             int option = input.nextInt();
             switch(option) {
                 case 1:
-                    disguise(admin);
-                    break;
-                case 2:
                     approvalMenu(admin);
                     break;
                 case 0:
@@ -40,14 +37,6 @@ public class AdminUI {
             }
         }
         input.close();
-    }
-    private void disguise(Admin admin) { // TODO (from brenn10): which requirement is this for?
-        // ask for what type of user you want to be as
-        System.out.println("WARNING: you must re-sign-in to access other Admin roles\n\n"
-                        +"What type of user would you like to sign into?");
-        // show all users of that type, ask which one you want to access
-        System.out.println("Which user would you like to disguise yourself as?");
-        // set current user to that, run that verification type
     }
 
     private void approvalMenu(Admin admin) {
