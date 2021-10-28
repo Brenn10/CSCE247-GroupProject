0d650441213275a42ac41bd6a424ad933f39779c
Stella Garcia <stellae13@gmail.com>
2021-10-28 09:13:26 -0400

    Finished admin ui rough draft

--- src/DataReader.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public abstract class DataReader {
-    public DataReader() {}
-
-    public abstract DataBlob read();
-}
--- src/DataWriter.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public abstract class DataWriter {
-    public DataWriter() {}
-
-    public abstract void write(ArrayList<User> users, ArrayList<Review> reviews, ArrayList<JobPosting> postings);
-}
