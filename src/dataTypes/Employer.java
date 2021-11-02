58a09102dc01a1d4b2857c3c52783c67ad909e1a
Stella Garcia <stellae13@gmail.com>
2021-11-02 10:30:06 -0400

    added javadoc

--- src/dataTypes/Employer.java
+++ src/dataTypes/Employer.java
@@ -8,12 +8,8 @@ import database.ReviewDatabase;
 import enums.JobPostingStatus;
 
 /**
-<<<<<<< HEAD
- * Employer User Data type of User
-=======
  * Employer User datatype
  * 
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
  * @author Ian McDevitt
  */
 public class Employer extends User {
@@ -22,21 +18,6 @@ public class Employer extends User {
     private ArrayList<Review> reviews;
     private double averageRating;
     private boolean removed;
-<<<<<<< HEAD
-/**
- * creates constructor for Employer User
- * @param id randomly generated refrence number 
- * @param username the name selcted by the user that will be displayed to others
- * @param password a keyword used for account safety
- * @param email an email address used for account verification
- * @param firstName the First name of the User
- * @param lastName the Last name of the User
- * @param approved if the account is approved by checking the email
- * @param company the company name 
- * @param averageRating the rating of the company 
- * @param removed if the company has been removed from vewing
- */
-=======
 
     /**
      * Constructor
@@ -52,7 +33,6 @@ public class Employer extends User {
      * @param averageRating the average rating of the employer
      * @param removed       boolean if they have been removed or not
      */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
     public Employer(UUID id, String username, String password, String email, String firstName, String lastName,
             boolean approved, String company, double averageRating, boolean removed) {
         super(id, username, password, email, firstName, lastName, approved, removed);
@@ -60,33 +40,18 @@ public class Employer extends User {
         this.averageRating = averageRating;
 
     }
-<<<<<<< HEAD
-/**
- * takes in a job and adds it to the job post datbase
- * @param job
- */
-=======
 
     /**
      * Make posting allows an employer to create a new job posting
      * 
      * @param job the posting they want to make
      */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
     public void makePosting(JobPosting job) {
         postings.add(job);
         JobPostingDatabase.getInstance().addPosting(job);
     }
 
     /**
-<<<<<<< HEAD
-     * overload make posting method that adds a job ot the jobposting database by taking the correct parameters of a job
-     * @param description a breif message describing the job
-     * @param requirements a short list of expectations 
-     * @param hourlyWage a double dispalying the wage
-     * @param status a boolean letting the user know if their application was apporved
-     * @param applicants a list of students who applied
-=======
      * Make posting allows an employer to create a new job posting overloaded method
      * 
      * @param description  a description of the job
@@ -94,7 +59,6 @@ public class Employer extends User {
      * @param hourlyWage   the hourly wage of the job
      * @param status       if the job is open, pending, or closed
      * @param applicants   an ArrayList of student applicants
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
      */
     public void makePosting(String description, ArrayList<String> requirements, double hourlyWage,
             JobPostingStatus status, ArrayList<Student> applicants) {
@@ -103,51 +67,14 @@ public class Employer extends User {
     }
 
     /**
-<<<<<<< HEAD
-     * takes in a job and string to change the desciption
-     * @param job a job
-     * @param toChange a string to change the description of the job
-=======
      * If the employer wants to change the description
      * 
      * @param job      the job they want to change
      * @param toChange what they want to description to get changed to
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
      */
     public void editDescription(JobPosting job, String toChange) {
         job.setDescription(toChange);
     }
-<<<<<<< HEAD
-/**
- * add news jobposting requirment to job
- * @param job
- * @param toChange
- */
-    public void addPostingRequirement(JobPosting job, String toChange) {
-        job.addRequirement(toChange);
-    }
-/**
- * removes a requirment from the jobposting passed in
- * @param job
- * @param toRemove
- */
-    public void removePostingRequirement(JobPosting job, String toRemove) {
-        job.removeRequriement(toRemove);
-    }
-/**
- * changes the wage of the jobpsoting passed in
- * @param job
- * @param wage
- */
-    public void editPostingWage(JobPosting job, double wage) {
-        job.setWage(wage);
-    }
-/**
- * changes the status of the jobposting passed in
- * @param job
- * @param status
- */
-=======
 
     /**
      * Add a new requirement to the ArrayList
@@ -185,45 +112,20 @@ public class Employer extends User {
      * @param job    the job they want to edit
      * @param status what they want to change the status to
      */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
     public void editPostingStatus(JobPosting job, JobPostingStatus status) {
         job.setStatus(status);
 
     }
-<<<<<<< HEAD
-/**
- * remove the current job posting 
- * @param job
- */
-=======
 
     /**
      * Remove a job posting
      * 
      * @param job posting the want to remove
      */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
     public void removePosting(JobPosting job) {
         JobPostingDatabase.getInstance().getPostings().remove(job);
         JobPostingDatabase.getInstance().getRemovedPostings().add(job);
     }
-<<<<<<< HEAD
-/**
- * takes in job postings and returns job posting applicants 
- * @param job
- * @return jobposting applicants
- */
-    public ArrayList<Student> viewApplicants(JobPosting job) {
-        return job.getApplicants();
-    }
-/**
- * takes in a student user, a score given, and a comment about the student
- * and adds a review to the current student
- * @param student
- * @param score
- * @param comment
- */
-=======
 
     /**
      * get applicants for a specific job
@@ -242,155 +144,31 @@ public class Employer extends User {
      * @param score   the score they want to give the student
      * @param comment a comment about a student
      */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
     public void rateStudent(Student student, int score, String comment) {
         Review rating = new Review.Builder().reviewer(this).reviewee(student).rating(score).comment(comment).build();
         ReviewDatabase.getInstance().addReview(rating);
     }
 
-<<<<<<< HEAD
-    /**
-     * returns the company name of Employer
-     * @return
-=======
     // get and set methods
 
     /**
      * Get method for company
      * 
      * @return the String company
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
      */
     public String getCompany() {
         return this.company;
     }
-<<<<<<< HEAD
-/**
- * returns currrent jobposting of Employer
- * @return
- */
-=======
 
     /**
      * Get method for postings
      * 
      * @return the ArrayList of postings
      */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
     public ArrayList<JobPosting> getPostings() {
         this.postings = JobPostingDatabase.getInstance().getPostingsByEmployer(this);
         return this.postings;
     }
-<<<<<<< HEAD
-/**
- * return ArrayList of reviews of Employer
- * @return
- */
-    public ArrayList<Review> getReviews() {
-        return this.reviews;
-    }
-/**
- * return the average rating of Employer
- * @return
- */
-    public double getAverageRating() {
-        return this.averageRating;
-    }
-/**
- * return the username of Employer
- * @return
- */
-    public String getUsername() {
-        return this.username;
-    }
-/**
- * returns the password of Employer
- * @return
- */
-    public String getPassword() {
-        return this.password;
-    }
-/**
- * returns email of Employer
- * @return
- */
-    public String getEmail() {
-        return this.email;
-    }
-/**
- * Set the name of the Company
- * @param company
- */
-    public void setCompany(String company) {
-        this.company = company;
-    }
-/**
- * set the current posting
- * @param postings
- */
-    public void setPostings(ArrayList<JobPosting> postings) {
-        this.postings = postings;
-    }
-/**
- * set the current Review
- * @param reviews
- */
-    public void setReviews(ArrayList<Review> reviews) {
-        this.reviews = reviews;
-    }
-/**
- * set the average Rating
- * @param rating
- */
-    public void setAverageRating(double rating) {
-        this.averageRating = rating;
-    }
-/**
- * set the username
- * @param username
- */
-    public void setUsername(String username) {
-        this.username = username;
-    }
-/**
- * set the password
- * @param password
- */
-    public void setPassword(String password) {
-        this.password = password;
-    }
-/**
- * set the email
- * @param email
- */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-/**
- * returns the boolean if removed
- * @return 
- */
-    public boolean isRemoved() {
-        return removed;
-    }
-/**
- * set the boolean value if removed
- */
-    public void setRemoved(boolean removed) {
-        this.removed = removed;
-    }
-/**
- * check if current  id == to an exisitng employer's id
- * @param employer
- * @return
- */
-    public boolean equals(Employer employer) {
-        return this.id == employer.getId();
-    }
-/**
- * a print string to print all accounnt information 
- */
-=======
 
     /**
      * Get method for reviews
@@ -534,7 +312,6 @@ public class Employer extends User {
      * 
      * @return String the employer printed
      */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append(this.getFullName() + "\n");
@@ -544,14 +321,9 @@ public class Employer extends User {
         sb.append(this.getAverageRating() + "\n");
         return sb.toString();
     }
-<<<<<<< HEAD
-/**
-     * Builder class allows for building a new admin
-=======
 
     /**
      * Builder class allows for building a new employer
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
      * 
      * @author Brennan Cain
      */
@@ -566,27 +338,14 @@ public class Employer extends User {
         private double averageRating;
         private String company;
         private boolean removed;
-<<<<<<< HEAD
-        /**
-        * Constructor sets all of the values to their default
-        */
-=======
 
         /**
          * Constructor sets all of the values to their default
          */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
         public Builder() {
             this.id = UUID.randomUUID();
             this.approved = false;
         }
-<<<<<<< HEAD
-/**
- * builds a new id for Employer
- * @param id
- * @return
- */
-=======
 
         /**
          * sets the ID
@@ -594,18 +353,10 @@ public class Employer extends User {
          * @param id what we want to set the ID to
          * @return the Builder with this ID
          */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
         public Builder id(UUID id) {
             this.id = id;
             return this;
         }
-<<<<<<< HEAD
-/**
- * builds a new Username for current Employer
- * @param username
- * @return
- */
-=======
 
         /**
          * sets the username
@@ -613,18 +364,10 @@ public class Employer extends User {
          * @param username what we want to set the username to
          * @return the Builder with this username
          */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
         public Builder username(String username) {
             this.username = username;
             return this;
         }
-<<<<<<< HEAD
-/**
- * builds a new password for current Employer
- * @param password
- * @return
- */
-=======
 
         /**
          * sets the password
@@ -632,18 +375,10 @@ public class Employer extends User {
          * @param password what we want to set the password to
          * @return the Builder with this password
          */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
         public Builder password(String password) {
             this.password = password;
             return this;
         }
-<<<<<<< HEAD
-/**
- * builds a new email for current Employer
- * @param email
- * @return
- */
-=======
 
         /**
          * sets the email
@@ -651,18 +386,10 @@ public class Employer extends User {
          * @param email what we want to set the email to
          * @return the Builder with this email
          */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
         public Builder email(String email) {
             this.email = email;
             return this;
         }
-<<<<<<< HEAD
-/**
- * builds a new first name for current Employer
- * @param firstName
- * @return
- */
-=======
 
         /**
          * sets the firstname
@@ -670,18 +397,10 @@ public class Employer extends User {
          * @param firstname what we want to set the firstname to
          * @return the Builder with this firstname
          */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
         public Builder firstName(String firstName) {
             this.firstName = firstName;
             return this;
         }
-<<<<<<< HEAD
-/**
- * builds a new last name for current Employer
- * @param lastName
- * @return
- */
-=======
 
         /**
          * sets the last name
@@ -689,18 +408,10 @@ public class Employer extends User {
          * @param lastname what we want to set the lastname to
          * @return the Builder with this lastname
          */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
         public Builder lastName(String lastName) {
             this.lastName = lastName;
             return this;
         }
-<<<<<<< HEAD
-/**
- * builds a approval status for current Employer
- * @param approved
- * @return
- */
-=======
 
         /**
          * sets the approved
@@ -708,18 +419,10 @@ public class Employer extends User {
          * @param approved what we want to set the approved to
          * @return the Builder with this email
          */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
         public Builder approved(boolean approved) {
             this.approved = approved;
             return this;
         }
-<<<<<<< HEAD
-/**
- * builds an  average rating for current Employer
- * @param averageRating
- * @return
- */
-=======
 
         /**
          * sets the averageRating
@@ -727,18 +430,10 @@ public class Employer extends User {
          * @param averageRating what we want to set the averageRating to
          * @return the Builder with this averageRating
          */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
         public Builder averageRating(double averageRating) {
             this.averageRating = averageRating;
             return this;
         }
-<<<<<<< HEAD
-/**
- * builds a new company name for Current Employer
- * @param company
- * @return
- */
-=======
 
         /**
          * sets the company
@@ -746,18 +441,10 @@ public class Employer extends User {
          * @param company what we want to set the company to
          * @return the Builder with this company
          */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
         public Builder company(String company) {
             this.company = company;
             return this;
         }
-<<<<<<< HEAD
-/**
- * builds a boolean value to be checked if removed for current Employer
- * @param removed
- * @return
- */
-=======
 
         /**
          * sets the removed
@@ -765,24 +452,16 @@ public class Employer extends User {
          * @param removed what we want to set the removed to
          * @return the Builder with this removed
          */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
         public Builder removed(boolean removed) {
             this.removed = removed;
             return this;
         }
-<<<<<<< HEAD
-/**
- * builds a new Employer for all parameterers
- * @return
- */
-=======
 
         /**
          * creates the new Employer user
          * 
          * @return a new Employer user
          */
->>>>>>> 5091e4c3250f45fbc0dc40aa658cd3094863c8e2
         public Employer build() {
             return new Employer(id, username, password, email, firstName, lastName, approved, company, averageRating,
                     removed);
--- src/dataTypes/Professor.java
+++ src/dataTypes/Professor.java
@@ -4,63 +4,143 @@ import java.util.ArrayList;
 import java.util.UUID;
 import database.ReviewDatabase;
 
+/**
+ * Professor User datatype
+ * 
+ * @author Ian McDevitt
+ */
 public class Professor extends User {
     private boolean removed;
     private ArrayList<Student> students;
 
+    /**
+     * Constructor
+     * 
+     * @param id        their unique identifier
+     * @param username  the account username
+     * @param password  the acoount passowrd
+     * @param email     the user's email
+     * @param firstName the user's first name
+     * @param lastName  the user's last name
+     * @param approved  if the user is approved by admin or not
+     * @param removed   if the user has been removed or not
+     */
     public Professor(UUID id, String username, String password, String email, String firstName, String lastName,
             boolean approved, boolean removed) {
         super(id, username, password, email, firstName, lastName, approved, removed);
     }
 
+    /**
+     * Create a rating for a student
+     * 
+     * @param student the student they want to rate
+     * @param rating  the rating they are giving
+     * @param comment a comment about the student
+     */
     public void rateStudent(Student student, int rating, String comment) {
         Review reviewToAdd = new Review.Builder().reviewer(this).reviewee(student).rating(rating).comment(comment)
                 .build();
         ReviewDatabase.getInstance().addReview(reviewToAdd);
     }
 
+    /**
+     * Gets an array of all the student reviews they have left
+     * 
+     * @param user the reviewer
+     * @return ArrayList of reviews
+     */
     public ArrayList<Review> getStudentReviewed(User user) {
         ArrayList<Review> reviews = new ArrayList<Review>();
         reviews = ReviewDatabase.getInstance().getReviewsByReviewer(user);
         return reviews;
     }
 
+    /**
+     * Get method for all of the students the professor has reviewed
+     * 
+     * @return ArrayList of students
+     */
     public ArrayList<Student> getStudents() {
         return this.students;
     }
 
+    /**
+     * Get method for username
+     * 
+     * @return the String username
+     */
     public String getUsername() {
         return this.username;
     }
 
+    /**
+     * Get method for password
+     * 
+     * @return the String password
+     */
     public String getPassword() {
         return this.password;
     }
 
+    /**
+     * Get method for email
+     * 
+     * @return the String email
+     */
     public String getEmail() {
         return this.email;
     }
 
+    /**
+     * Set method for username
+     * 
+     * @param username what we want to set the username to
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Set method for password
+     * 
+     * @param password what we want to set the password to
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Set method for email
+     * 
+     * @param email what we want to set teh email to
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Get method for removed
+     * 
+     * @return boolean if the professor has been removed or not
+     */
     public boolean isRemoved() {
         return removed;
     }
 
+    /**
+     * Set method for removed
+     * 
+     * @return the boolean removed we want to set it to
+     */
     public void setRemoved(boolean removed) {
         this.removed = removed;
     }
 
+    /**
+     * toString method
+     * 
+     * @return String we want to print
+     */
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append(this.getFullName() + "\n");
@@ -69,10 +149,21 @@ public class Professor extends User {
         return sb.toString();
     }
 
+    /**
+     * Equals method
+     * 
+     * @param professor we want to compare to
+     * @return true if they are the same user, false otherwise
+     */
     public boolean equals(Professor professor) {
         return this.id == professor.getId();
     }
 
+    /**
+     * Builder class allows for building a new Professor
+     * 
+     * @author Brennan Cain
+     */
     public static class Builder {
         private UUID id;
         private String username;
@@ -83,50 +174,106 @@ public class Professor extends User {
         private boolean approved;
         private boolean removed;
 
+        /**
+         * Constructor sets default values
+         */
         public Builder() {
             this.id = UUID.randomUUID();
         }
 
+        /**
+         * sets the ID
+         * 
+         * @param id what we want to set the ID to
+         * @return a Builder with that ID
+         */
         public Builder id(UUID id) {
             this.id = id;
             return this;
         }
 
+        /**
+         * sets the username
+         * 
+         * @param username what we want to set the username to
+         * @return a Builder with that username
+         */
         public Builder username(String username) {
             this.username = username;
             return this;
         }
 
+        /**
+         * sets the password
+         * 
+         * @param password what we want to set the password to
+         * @return a Builder with that password
+         */
         public Builder password(String password) {
             this.password = password;
             return this;
         }
 
+        /**
+         * sets the email
+         * 
+         * @param email what we want to set the email to
+         * @return a Builder with that email
+         */
         public Builder email(String email) {
             this.email = email;
             return this;
         }
 
+        /**
+         * sets the firstName
+         * 
+         * @param firstName what we want to set the firstName to
+         * @return a Builder with that first name
+         */
         public Builder firstName(String firstName) {
             this.firstName = firstName;
             return this;
         }
 
+        /**
+         * sets the lastName
+         * 
+         * @param lastName what we want to set the lastName to
+         * @return a Builder with that lastname
+         */
         public Builder lastName(String lastName) {
             this.lastName = lastName;
             return this;
         }
 
+        /**
+         * sets the approved boolean
+         * 
+         * @param approved if the user is approved or not
+         * @return a Builder with that approval status
+         */
         public Builder approved(boolean approved) {
             this.approved = approved;
             return this;
         }
 
+        /**
+         * sets the removed boolean
+         * 
+         * @param removed if the user is removed or not
+         * @return a Builder with that removal status
+         */
         public Builder removed(boolean removed) {
             this.removed = removed;
             return this;
         }
 
+        /**
+         * creates the new admin user
+         * 
+         * @return a new Admin user
+         */
         public Professor build() {
             return new Professor(id, username, password, email, firstName, lastName, approved, removed);
         }
