5fcc9471ff70aca9b15502c5697530adafeb95ff
IanMcDevitt <mcdeviti@email.sc.edu>
2021-11-11 10:16:46 -0500

    Skeletion files for non-User DataType Testing files

---
+++ src/test/DataBlobTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DataBlobTest {
+
+    @BeforeClass
+    public static void oneTimeSetup() {
+
+    }
+
+    @AfterClass
+    public static void onTimeTearDown() {
+
+    }
+
+    @BeforeEach
+    public static void setup() {
+
+    }
+
+    @AfterEach static void tearDown() {
+
+    }
+
+    @Test public void 
+    
+}
