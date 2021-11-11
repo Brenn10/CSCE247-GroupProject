24241ea0b33e866ac5b4ab93841d57a8cb86737f
Brennan Cain <brenn.cain@gmail.com>
2021-11-11 17:29:09 -0500

    add student testing

---
+++ src/test/StudentTest.java
@@ -0,0 +1,53 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+
+import dataTypes.Education;
+import dataTypes.Employment;
+import dataTypes.Student;
+
+public class StudentTest {
+    @Test
+    public void addEmployment() {
+        Student student = new Student.Builder().build();
+        Employment employment = new Employment.Builder().build();
+        student.addEmployment(employment);
+        assertTrue(student.getEmployments().contains(employment));
+    }
+
+    @Test
+    public void testAddTechnicalSkill() {
+        Student student = new Student.Builder().build();
+        student.addTechincalSkill("Java");
+        assertTrue(student.getTechnicalSkills().contains("Java"));
+    }
+
+    @Test
+    public void testRemoveTechnicalSkill() {
+        Student student = new Student.Builder().build();
+        student.addTechincalSkill("Java");
+        student.removeTechincalSkill("Java");
+        assertTrue(student.getTechnicalSkills().isEmpty());
+    }
+
+    @Test
+    public void testAddEducation() {
+        Student student = new Student.Builder().build();
+        Education education = new Education.Builder().build();
+        student.addEducation(education);
+        assertTrue(student.getEducations().contains(education));
+    }
+
+    @Test
+    public void testRemoveEducation() {
+        Student student = new Student.Builder().build();
+        Education education = new Education.Builder().build();
+        student.addEducation(education);
+        student.removeEducation(education);
+        assertTrue(student.getEducations().isEmpty());
+    }
+
+    
+}
