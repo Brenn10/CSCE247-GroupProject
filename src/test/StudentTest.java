4f2aceddb94c401d374547fc5a4b669bc0d53b07
Brennan Cain <brenn.cain@gmail.com>
2021-11-11 20:07:41 -0500

    Updated student test

--- src/test/StudentTest.java
+++ src/test/StudentTest.java
@@ -2,6 +2,8 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import dataTypes.Education;
@@ -11,7 +13,11 @@ import dataTypes.Student;
 public class StudentTest {
     @Test
     public void addEmployment() {
-        Student student = new Student.Builder().build();
+        Student student = new Student.Builder()
+                                     .educations(new ArrayList<Education>())
+                                     .employments(new ArrayList<Employment>())
+                                     .technicalSkills(new ArrayList<String>())
+                                     .build();
         Employment employment = new Employment.Builder().build();
         student.addEmployment(employment);
         assertTrue(student.getEmployments().contains(employment));
@@ -19,14 +25,22 @@ public class StudentTest {
 
     @Test
     public void testAddTechnicalSkill() {
-        Student student = new Student.Builder().build();
+        Student student = new Student.Builder()
+        .educations(new ArrayList<Education>())
+        .employments(new ArrayList<Employment>())
+        .technicalSkills(new ArrayList<String>())
+        .build();
         student.addTechincalSkill("Java");
         assertTrue(student.getTechnicalSkills().contains("Java"));
     }
 
     @Test
     public void testRemoveTechnicalSkill() {
-        Student student = new Student.Builder().build();
+        Student student = new Student.Builder()
+        .educations(new ArrayList<Education>())
+        .employments(new ArrayList<Employment>())
+        .technicalSkills(new ArrayList<String>())
+        .build();
         student.addTechincalSkill("Java");
         student.removeTechincalSkill("Java");
         assertTrue(student.getTechnicalSkills().isEmpty());
@@ -34,7 +48,11 @@ public class StudentTest {
 
     @Test
     public void testAddEducation() {
-        Student student = new Student.Builder().build();
+        Student student = new Student.Builder()
+        .educations(new ArrayList<Education>())
+        .employments(new ArrayList<Employment>())
+        .technicalSkills(new ArrayList<String>())
+        .build();
         Education education = new Education.Builder().build();
         student.addEducation(education);
         assertTrue(student.getEducations().contains(education));
@@ -42,12 +60,16 @@ public class StudentTest {
 
     @Test
     public void testRemoveEducation() {
-        Student student = new Student.Builder().build();
+        Student student = new Student.Builder()
+        .educations(new ArrayList<Education>())
+        .employments(new ArrayList<Employment>())
+        .technicalSkills(new ArrayList<String>())
+        .build();
         Education education = new Education.Builder().build();
         student.addEducation(education);
         student.removeEducation(education);
         assertTrue(student.getEducations().isEmpty());
     }
 
-    
+
 }
